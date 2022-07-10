8f97b491df7ccb3534eeeebba5546b8ecd7c656e
Maxime Toussaint <39208189+MaxDude132@users.noreply.github.com>
2022-07-10 15:36:39 -0400

    Update settings.py

--- witcher_tabletop_api/settings.py
+++ witcher_tabletop_api/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +26,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--3+$9rqj_*md%bc0ki(xb%n=gs)vkj)#^0_i31%fojn1nsuavm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['witcher-tabletop-api.herokuapp.com']
 
