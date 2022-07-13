e01bdbbd6d932e98d4f1f2c426dea95c555f2f32
Maxime Toussaint <39208189+MaxDude132@users.noreply.github.com>
2022-07-12 20:27:25 -0400

    Update utils.py

--- character/models/utils.py
+++ character/models/utils.py
@@ -5,3 +5,6 @@ class DiceRollInformation(models.Model):
     number_of_dice = models.IntegerField()
     number_of_sides = models.IntegerField()
     modifier = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.number_of_dice}d{self.number_of_sides}{'+{}'.format(self.modifier) if self.modifier else ''}"
