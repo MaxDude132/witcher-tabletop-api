fe120f40c6cf9d6dcea5a107816add0910b017bd
Maxime Toussaint <39208189+MaxDude132@users.noreply.github.com>
2022-07-12 20:55:39 -0400

    Add constraint to DiceRollInformation

--- character/models/utils.py
+++ character/models/utils.py
@@ -4,7 +4,10 @@ from django.db import models
 class DiceRollInformation(models.Model):
     number_of_dice = models.IntegerField()
     number_of_sides = models.IntegerField()
-    modifier = models.IntegerField()
+    modifier = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return f"{self.number_of_dice}d{self.number_of_sides}{'+{}'.format(self.modifier) if self.modifier else ''}"
+
+    class Meta:
+        unique_together = ('number_of_dice', 'number_of_sides', 'modifier')
